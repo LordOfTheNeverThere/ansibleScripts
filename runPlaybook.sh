bbe0088b367eb4c9932581afd4bb3251abbc43d6
miguel <1241690@isep.ipp.pt>
2025-12-19 18:45:01 +0000

    Created role and tasks used for yum package management

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -1,1 +1,2 @@
+ansible-playbook -i production.yml installRequiredPackagesOnAdminNode.yml --ask-vault-pass -vvv
 ansible-playbook -i production.yml provisioningNetworkDevices.yml --ask-vault-pass -vvv
