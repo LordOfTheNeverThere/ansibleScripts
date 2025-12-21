4a93896f9e33d2ec9ac22267078f1db858fb72ff
miguel <1241690@isep.ipp.pt>
2025-12-21 21:48:22 +0000

    Debugging NFS Container

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -1,2 +1,4 @@
-ansible-playbook -i production.yml installRequiredPackagesOnAdminNode.yml --ask-vault-pass -vvv
+
 ansible-playbook -i production.yml provisioningNetworkDevices.yml --ask-vault-pass -vvv
+ansible-playbook -i production.yml installRequiredPackagesOnAdminNode.yml --ask-vault-pass -vvv
+ansible-playbook -i production.yml creatingNFSNASStorage.yml -vvv --ask-vault-pass
